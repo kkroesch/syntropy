ec791c729dd5d2c486d8957add2c1435deb0c1fd
Karsten Kroesch <karsten@aton.kroesch.lan>
2026-08-30 08:44:46 +0200

    Automated sync.

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ ln -s ${PWD}/syntropy/.zshenv ${HOME}
 ln -s ${PWD}/syntropy/.alias ${HOME}
 ln -s ${PWD}/syntropy/.config/git.plugin.zsh ${HOME}/.config/
 
+cp ${PWD}/syntropy/.ssh/config ${HOME}/.ssh/
+
 eget direnv/direnv -to ${HOME}/.local/bin
 eget direnv/direnv --to ${HOME}/.local/bin
 eget junegunn/fzf --to ${HOME}/.local/bin
