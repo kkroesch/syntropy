14ef9ed5b7c9da3fd86b18a609ba74b4b9019721
Karsten Kroesch <karsten@aton.kroesch.lan>
2026-08-30 08:20:04 +0200

    Automated sync.

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
-ln -s $(PWD)/syntropy/.zshrc $(HOME)
-ln -s $(PWD)/syntropy/.zshenv $(HOME)
-ln -s $(PWD)/syntropy/.alias $(HOME)
-ln -s $(PWD)/syntropy/.config/git.plugin.zsh $(HOME)/.config/
+ln -s ${PWD}/syntropy/.zshrc ${HOME}
+ln -s ${PWD}/syntropy/.zshenv ${HOME}
+ln -s ${PWD}/syntropy/.alias ${HOME}
+ln -s ${PWD}/syntropy/.config/git.plugin.zsh ${HOME}/.config/
 
-eget direnv/direnv -to $(HOME)/.local/bin
-eget direnv/direnv --to $(HOME)/.local/bin
-eget junegunn/fzf --to $(HOME)/.local/bin
-eget ajeetdsouza/zoxide --to $(HOME)/.local/bin
-eget eza-community/eza --to $(HOME)/.local/bin
+eget direnv/direnv -to ${HOME}/.local/bin
+eget direnv/direnv --to ${HOME}/.local/bin
+eget junegunn/fzf --to ${HOME}/.local/bin
+eget ajeetdsouza/zoxide --to ${HOME}/.local/bin
+eget eza-community/eza --to ${HOME}/.local/bin
+
+systemctl --user enable --now ssh-agent.service
